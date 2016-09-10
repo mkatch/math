b33e17c91f6f66d1605b42629ebb20c4848c5fc1
Marcin Kaczmarek <mkacz91@gmail.com>
2016-09-10 15:31:36 +0200

    Provide explicit overload of abs for int
    Clang refuses to compile otherwise.

--- special_functions.h
+++ special_functions.h
@@ -18,6 +18,7 @@ using std::floor;
 #undef min
 #undef max
 
+inline int abs(int x) { return x >= 0 ? x : -x; }
 inline float log(float b, float x) { return log(x) / log(b); }
 inline float sgn(float x) { return signbit(x) ? -1.0f : 1.0f; }
 inline double sgn(double x) { return signbit(x) ? -1.0 : 1.0; }
