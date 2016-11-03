165af5805d3ac540d03992e153cbfb5d96e04fad
Marcin Kaczmarek <mkacz91@gmail.com>
2016-11-03 16:49:53 +0100

    Refactor vec2 type traits

--- vec2.h
+++ vec2.h
@@ -9,6 +9,10 @@ namespace kletch {
 template <class T>
 struct vec2
 {
+    typedef T scalar_t;
+    typedef common_t<T, float> float_t;
+    static constexpr int COMPONENT_COUNT = 2;
+
     T x;
     T y;
 
@@ -16,9 +20,7 @@ struct vec2
     constexpr vec2(T x, T y) : x(x), y(y) { }
     constexpr vec2(T value) : x(value), y(value) { }
     constexpr vec2(vec2 const& v) : x(v.x), y(v.y) { }
-
-    template <class T1, class T2>
-    vec2(vec2<T1> const& u, vec2<T2> const& v) : x(v.x - u.x), y(v.y - u.y) { }
+    constexpr vec2(vec2 const& u, vec2 const& v) : x(v.x - u.x), y(v.y - u.y) { }
 
     vec2& operator = (vec2 const& v) { x = v.x; y = v.y; return *this; }
     vec2& operator += (vec2 const& v) { x += v.x; y += v.y; return *this; }
@@ -32,16 +34,22 @@ struct vec2
     template<class S> operator vec2<S> () const { return vec2<S>(x, y); }
 
     vec2& norm() { return *this /= len(); }
-    vec2& rot(common_t<T, float> angle);
+    vec2& rot(float_t angle);
 
     T len_sq() const { return x * x + y * y; }
-    common_t<T, float> len() const { return hypot(x, y); }
-    common_t<T, float> angle() const { return atan2(y, x); }
+    float_t len() const { return hypot(x, y); }
+    float_t angle() const { return atan2(y, x); }
     vec2 lhp() const { return vec2(-y, x); }
     vec2 rhp() const { return vec2(y, -x); }
     vec2 lhn() const { return lhp().norm(); }
     vec2 rhn() const { return rhp().norm(); }
-    vec2<common_t<T, float>> dir() const { vec2<common_t<T, float>> v = *this; return v.norm(); }
+    vec2<float_t> dir() const { vec2<float_t> v = *this; return v.norm(); }
+
+    static vec2 span(vec2 const& u, vec2 const& v) { return vec2(u, v); }
+    static T dot(vec2 const& u, vec2 const& v) { return u.x * v.x + u.y * v.y; }
+    static T per(vec2 const& u, vec2 const& v) { return u.x * v.y - u.y * v.x; }
+    static T dist_sq(vec2 const& u, vec2 const& v) { return span(u, v).len_sq(); }
+    static float_t dist(vec2 const& u, vec2 const& v) { return span(u, v).len(); }
 
     template <class S> vec2<S> of(vec2<S> const& unx, vec2<S> const& uny) const;
     template <class S> vec2<S> in(vec2<S> const& unx, vec2<S> const& uny) const;
@@ -105,28 +113,31 @@ template <class S, class T> inline
 vec2c<S, T> operator / (vec2<S> const& u, vec2<T> const& v) { vec2c<S, T> c = u; return c /= v;}
 
 template <class T> inline T len_sq(vec2<T> const& v) { return v.len_sq(); }
-template <class T> inline common_t<T, float> len(vec2<T> const& v) { return v.len(); }
-template <class T> inline common_t<T, float> angle(vec2<T> const& v) { return v.angle(); }
+template <class T> inline typename vec2<T>::float_t len(vec2<T> const& v) { return v.len(); }
+template <class T> inline typename vec2<T>::float_t angle(vec2<T> const& v) { return v.angle(); }
 template <class T> inline vec2<T> lhp(vec2<T> const& v) { return v.lhp(); }
 template <class T> inline vec2<T> rhp(vec2<T> const& v) { return v.rhp(); }
 template <class T> inline vec2<T> lhn(vec2<T> const& v) { return v.lhn(); }
 template <class T> inline vec2<T> rhn(vec2<T> const& v) { return v.rhn(); }
-template <class T> inline vec2<T> rot(common_t<T, float> a, vec2<T> v) { return v.rot(a); }
+template <class T> inline vec2<T> rot(typename vec2<T>::float_t a, vec2<T> v) { return v.rot(a); }
 
 template <class S, class T> inline
-common_t<S, T> dot(vec2<S> const& u, vec2<T> const& v) { return u.x * v.x + u.y * v.y; }
+vec2c<S, T> span(vec2<S> const& u, vec2<T> const& v) { return vec2c<S, T>::span(u, v); }
 
 template <class S, class T> inline
-common_t<S, T> per(vec2<S> const& u, vec2<T> const& v) { return u.x * v.y - u.y * v.x; }
+common_t<S, T> dot(vec2<S> const& u, vec2<T> const& v) { return vec2c<S, T>::dot(u, v); }
 
 template <class S, class T> inline
-common_t<S, T> dist_sq(vec2<S> const& u, vec2<T> const& v) { return span(u, v).len_sq(); }
+common_t<S, T> per(vec2<S> const& u, vec2<T> const& v) { return vec2c<S, T>::per(u, v); }
 
 template <class S, class T> inline
-common_t<S, T> dist(vec2<S> const& u, vec2<T> const& v) { return span(u, v).len(); }
+common_t<S, T> dist_sq(vec2<S> const& u, vec2<T> const& v) { return vec2c<S, T>::dist_sq(u, v); }
 
 template <class S, class T> inline
-vec2c<S, T> span(vec2<S> const& u, vec2<T> const& v) { return vec2c<S, T>(u, v); }
+typename vec2c<S, T>::float_t dist(vec2<S> const& u, vec2<T> const& v)
+{
+    return vec2c<S, T>::dist(u, v);
+}
 
 template <class T> inline
 vec2c<T, float> dir(vec2<T> const& v) { v.dir(); }
@@ -146,7 +157,7 @@ vec2<S> vec2<T>::of(vec2<S> const& unx, vec2<S> const& uny) const
 template <class T> template <class S> inline
 vec2<S> vec2<T>::in(vec2<S> const& unx, vec2<S> const& uny) const
 {
-     return { dot(*this, unx), dot(*this, uny) };
+    return { dot(*this, unx), dot(*this, uny) };
 }
 
 template <class T> inline
