7d4d5ee6a9e09401bc14c89ae2f1a1d7e6f1d98b
Marcin Kaczmarek <mkacz91@gmail.com>
2016-11-05 16:02:21 +0100

    Rename vec2 operator [] argument

--- vec2.h
+++ vec2.h
@@ -33,7 +33,7 @@ struct vec2
     vec2& operator /= (vec2 const& v) { x /= v.x; y /= v.y; return *this; }
     vec2& operator /= (T s) { return *this *= 1.0f / s; }
 
-    T operator [] (int component) const { return *(reinterpret_cast<T const*>(this) + component); }
+    T operator [] (int i) const { return *(reinterpret_cast<T const*>(this) + i); }
     template<class S> operator vec2<S> () const { return vec2<S>(x, y); }
 
     vec2& norm() { return *this /= len(); }
