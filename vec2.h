9996a7288c9a1f97b7b10242069a911aa3e00c5c
Marcin Kaczmarek <mkc@orgadata.com>
2016-11-18 09:28:46 +0100

    Add comprehensive vec2::normalize and rotate

--- vec2.h
+++ vec2.h
@@ -36,19 +36,29 @@ struct vec2
     T operator [] (int i) const { return *(reinterpret_cast<T const*>(this) + i); }
     template<class S> operator vec2<S> () const { return vec2<S>(x, y); }
 
-    vec2& norm() { return *this /= len(); }
-    vec2& rot(float_t angle);
-
+    vec2& normalize() { return *this /= len(); }
+    vec2 dir() const { return vec2(*this).normalize(); }
+    vec2& rotate(float_t angle) { return rotate(sin(angle), cos(angle)); }
+    vec2& rotate(float_t s, float_t c) { return *this = { c * x - s * y, s * x + c * y }; }
+    vec2& rotate(vec2<float_t> const& rotation) { return rotate(rotation.y, rotation.x); }
+    vec2 rotated(float_t angle) const { vec(*this).rotate(angle); }
+    vec2 rotated(float_t s, float_t c) const { vec2(*this).rotate(s, c); }
+    vec2 rotated(vec2<float_t> const& rot) const{ vec2(*this).rotate(rot); }
     T len_sq() const { return x * x + y * y; }
     float_t len() const { return hypot(x, y); }
     float_t angle() const { return atan2(y, x); }
     vec2 lhp() const { return vec2(-y, x); }
     vec2 rhp() const { return vec2(y, -x); }
-    vec2 lhn() const { return lhp().norm(); }
-    vec2 rhn() const { return rhp().norm(); }
-    vec2<float_t> dir() const { vec2<float_t> v = *this; return v.norm(); }
+    vec2 lhn() const { return lhp().normalize(); }
+    vec2 rhn() const { return rhp().normalize(); }
 
     static vec2 span(vec2 const& u, vec2 const& v) { return vec2(u, v); }
+    static vec2<float_t> dir(vec2 const& v) { return vec2<float_t>(v).dir(); }
+    static vec2<float_t> dir(vec2 const& u, vec2 const& v) { return vec2<float_t>(u, v).dir(); }
+    static vec2 dir(float_t angle) { return vec2(cos(angle), sin(angle)); }
+    static vec2<float_t> rotate(vec2 const& v, float_t angle);
+    static vec2<float_t> rotate(vec2 const& v, float_t s, float_t c);
+    static vec2<float_t> rotate(vec2 const& v, vec2<float_t> const& rot);
     static T dot(vec2 const& u, vec2 const& v) { return u.x * v.x + u.y * v.y; }
     static T per(vec2 const& u, vec2 const& v) { return u.x * v.y - u.y * v.x; }
     static T dist_sq(vec2 const& u, vec2 const& v) { return span(u, v).len_sq(); }
@@ -67,6 +77,8 @@ struct vec2
 };
 
 template <class S, class T> using vec2c = vec2<common_t<S, T>>;
+template <class T> using vec2_float_t = typename vec2<T>::float_t;
+template <class S, class T> using vec2c_float_t = typename vec2c<S, T>::float_t;
 template <class T> constexpr vec2<T> zero2() { return vec2<T>(0, 0); }
 template <class T> constexpr vec2<T> unx2() { return vec2<T>(1, 0); }
 template <class T> constexpr vec2<T> uny2() { return vec2<T>(0, 1); }
@@ -123,10 +135,57 @@ template <class T> inline vec2<T> lhp(vec2<T> const& v) { return v.lhp(); }
 template <class T> inline vec2<T> rhp(vec2<T> const& v) { return v.rhp(); }
 template <class T> inline vec2<T> lhn(vec2<T> const& v) { return v.lhn(); }
 template <class T> inline vec2<T> rhn(vec2<T> const& v) { return v.rhn(); }
-template <class T> inline vec2<T> rot(typename vec2<T>::float_t a, vec2<T> v) { return v.rot(a); }
 
 template <class S, class T> inline
 vec2c<S, T> span(vec2<S> const& u, vec2<T> const& v) { return vec2c<S, T>::span(u, v); }
+
+template <class T> inline
+vec2<vec2_float_t<T>> dir(vec2<T> const& v) { return vec2<vec2_float_t<T>>::dir(v); }
+
+template <class S, class T> inline
+vec2<vec2c_float_t<S, T>> dir(vec2<S> const& u, vec2<T> const& v)
+{
+    return vec2<vec2c_float_t<S, T>>::dir(u, v);
+}
+
+template <class T> inline
+vec2<vec2_float_t<T>> dir(T angle) { return vec2<vec2_float_t<T>>::dir(angle); }
+
+template <class T> inline
+vec2<vec2_float_t<T>> vec2<T>::rotate(vec2<T> const& v, vec2_float_t<T> angle)
+{
+     return vec2<vec2_float_t<T>>(v).rotate(angle);
+}
+
+template <class T> inline
+vec2<vec2_float_t<T>> vec2<T>::rotate(vec2<T> const& v, vec2_float_t<T> s, vec2_float_t<T> c)
+{
+     return vec2<vec2_float_t<T>>(v).rotate(s, c);
+}
+
+template <class T> inline
+vec2<vec2_float_t<T>> vec2<T>::rotate(vec2<T> const& v, vec2<vec2_float_t<T>> const& rot)
+{
+     return vec2<vec2_float_t<T>>(v).rotate(rot);
+}
+
+template <class S, class T> inline
+vec2<vec2c_float_t<S, T>> rotate(vec2<S> const& u, T angle)
+{
+    return vec2<vec2c_float_t<S, T>>::rotate(u, angle);
+}
+
+template <class S, class T> inline
+vec2<vec2c_float_t<S, T>> rotate(vec2<S> const& u, T s, T c)
+{
+    return vec2<vec2c_float_t<S, T>>::rotate(u, s, c);
+}
+
+template <class S, class T> inline
+vec2<vec2c_float_t<S, T>> rotate(vec2<S> const& u, vec2<T> const& rot)
+{
+    return vec2<vec2c_float_t<S, T>>::rotate(u, rot);
+}
 
 template <class S, class T> inline
 common_t<S, T> dot(vec2<S> const& u, vec2<T> const& v) { return vec2c<S, T>::dot(u, v); }
@@ -146,15 +205,6 @@ typename vec2c<S, T>::float_t dist(vec2<S> const& u, vec2<T> const& v)
 template <class S, class T> inline
 vec2c<S, T> mid(vec2<S> const& u, vec2<T> const& v) { return vec2c<S, T>::mid(u, v); }
 
-template <class T> inline
-vec2c<T, float> dir(vec2<T> const& v) { v.dir(); }
-
-template <class S, class T> inline
-vec2c<T, float> dir(vec2<S> const& u, vec2<T> const& v) { return span(u, v).dir(); }
-
-template <class T> inline
-vec2<T> dir(T angle) { return vec2<T>(cos(angle), sin(angle)); }
-
 template <class T> template<class S> inline
 vec2<S> vec2<T>::of(vec2<S> const& unx, vec2<S> const& uny) const
 {
@@ -165,13 +215,6 @@ template <class T> template <class S> inline
 vec2<S> vec2<T>::in(vec2<S> const& unx, vec2<S> const& uny) const
 {
     return { dot(*this, unx), dot(*this, uny) };
-}
-
-template <class T> inline
-vec2<T>& vec2<T>::rot(common_t<T, float> angle)
-{
-    auto s = sin(angle), c = cos(angle);
-    return *this = { c * x - s * y, s * x + c * y };
 }
 
 typedef vec2<int> vec2i;
