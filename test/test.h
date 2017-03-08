dd17232de3018daf0defcded0f74baeba0a53cd3
Marcin Kaczmarek <mkacz91@gmail.com>
2017-03-08 21:32:26 +0100

    Add NUMERIC_TEST_CASE
    It is a special test case that executes for both float and double
    scalar type as parameter.

--- test/test.h
+++ test/test.h
@@ -3,4 +3,16 @@
 
 #include "catch/single_include/catch.hpp"
 
+#define NUMERIC_TEST_CASE2(name, tags, runner_name) \
+    template <class T> void runner_name(); \
+    TEST_CASE(name, tags) \
+    { \
+        SECTION( "With float as scalar type") { runner_name<float>(); } \
+        SECTION("With double as scalar type") { runner_name<double>(); } \
+    } \
+    template <class T> void runner_name()
+
+#define NUMERIC_TEST_CASE(name, tags) \
+    NUMERIC_TEST_CASE2(name, tags, INTERNAL_CATCH_UNIQUE_NAME(____N_U_M_E_R_I_C____T_E_S_T____))
+
 #endif // JOGURT_MATH_TEST_H
